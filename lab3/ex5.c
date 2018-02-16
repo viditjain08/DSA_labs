242ef96d7355a30dbe8c39bdbeb9c5f3b9c7345c
viditjain08 <viditjain08@gmail.com>
2018-02-16 14:52:45 +0530

    Lab 3: Improved q5 algo

--- lab3/ex5.c
+++ lab3/ex5.c
@@ -20,39 +20,34 @@ tempptr makeCircularList(tempptr Ls) {
 		return Ls;
 
 	} else {
-
-		tempptr a;
-		a.size = sizeof(struct list);
 		List l = Ls.ptr;
-		Element temp = l->head;
-		if(temp==temp->next) {
-			return Ls;
+		Element hare = l->head->next;
+		Element tort = l->head;
+		for(;hare->next!=tort&&hare!=tort;hare=hare->next,tort=tort->next) {
+			hare=hare->next;
 		}
 
-		int flag=0;
-
-		while(1) {
-			temp=l->head;
-			Element tnext = temp->next;
-			Element n = temp->next;
-			Element n2;
-			for(;n!=l->head;temp=n, n=n2) {
-				n2=n->next;
-				n->next=temp;
-			}
-			n->next=temp;
-			if(temp==tnext) {
-				a.ptr=l->head;
-				myfree(a);
-				l->head=tnext;
-			}	else {
-				if(flag%2!=0) {
-					int x = testCyclic(Ls);
-				}
-				printf("Heap size after making list completely circular- %d\n", heap);
-				return Ls;
-			}
-			flag++;
+		int count=1;
+		Element temp = tort->next;
+		while(temp!=tort) {
+			count++;
+			temp=temp->next;
 		}
+		temp = l->head;
+		Element temp2 = l->head;
+		for(int i=0;i<count;i++) {
+			temp=temp->next;
+		}
+		tempptr a;
+		a.size = sizeof(struct element);	
+		while(temp2!=temp) {
+			a.ptr = temp2;
+			temp2=temp2->next;
+			temp=temp->next;
+			l->head = temp2;
+			myfree(a);
+		}
+		printf("Heap size after making list completely circular- %d\n", heap);
+		return Ls;	
 	}
 }
